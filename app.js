e216377cec50bee0a68ede23ea1c94a101a10c1b
AC Roselee <acroselee@gmail.com>
2019-01-16 13:29:57 -0800

    Safety commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+
+// var Kale = () => (
+//   <li>kale</li>
+// );
+// var Cheese = () => (
+//   <li>cheese</li>
+// );
+
+var GroceryListItem = (props) => (
+ <ul>
+ <li>{props.groceryItems[0]}</li>
+  <li>{props.groceryItems[1]}</li>
+ </ul>
+);
+
+var GroceryList = () => (
+  <div>
+    <GroceryListItem groceryItems={['kale', 'cheese']}/>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
